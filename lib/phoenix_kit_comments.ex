897c3c6070569f2ae7f58aeeef3e5a687629c0ba
Dmitri Don <fotkin@gmail.com>
2026-05-11 22:23:42 +0000

    Bump version to 0.2.0

--- lib/phoenix_kit_comments.ex
+++ lib/phoenix_kit_comments.ex
@@ -269,7 +269,7 @@ defmodule PhoenixKitComments do
   def module_name, do: "Comments"
 
   @impl PhoenixKit.Module
-  def version, do: "0.1.5"
+  def version, do: "0.2.0"
 
   @impl PhoenixKit.Module
   def permission_metadata do
